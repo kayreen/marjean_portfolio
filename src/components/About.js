ee00497388ace23416f884b4ffe50c56333013ff
kayreen <153533580+kayreen@users.noreply.github.com>
2024-12-15 19:32:20 +0800

    Update About.js

--- src/components/About.js
+++ src/components/About.js
@@ -18,7 +18,7 @@ const About = () => {
       {/* Buttons for View and Download */}
       <div style={{ marginTop: "20px", textAlign: "center" }}>
         <a
-          href={`${process.env.PUBLIC_URL}/MarJeanRoxas_CV.pdf`}
+          href="/MarJeanRoxas_CV.pdf"
           target="_blank" // Opens in a new tab
           rel="noopener noreferrer"
           className="view-btn"
